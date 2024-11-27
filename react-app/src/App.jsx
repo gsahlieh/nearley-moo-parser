51bc2590c4249f1717b9b736afd0311065c4dff0
Gabriel Sahlieh <gsahlieh@outlook.com>
2024-11-27 21:19:07 +1100

    v1.7

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import icon from "/icon.png";
 import "./App.css";
 import ExpressionEvaluator from "./ExpressionEvaluator";
 
 function App() {
   return (
-    <div className="space-y-4 min-h-[30vh]">
+    <div className="space-y-4 min-h-[50vh] flex flex-col items-center">
+      <div className="text-center">
+        <img className="w-[96px]" src={icon} alt="" />
+      </div>
       <h1>Expression Evaluator</h1>
 
       <ExpressionEvaluator />
