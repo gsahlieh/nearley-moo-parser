f4fa685ff39e7ae544c56cfdcc9e405c0e8e0e26
Gabriel Sahlieh <gsahlieh@outlook.com>
2024-11-27 21:12:21 +1100

    v1.5

--- react-app/src/parser/grammar.js
+++ react-app/src/parser/grammar.js
@@ -3,117 +3,110 @@
 
 import lexer from "./lexer";
 
-(function () {
-  function id(x) {
-    return x[0];
-  }
+function id(x) {
+  return x[0];
+}
 
-  var grammar = {
-    Lexer: lexer,
-    ParserRules: [
-      {
-        name: "comparison",
-        symbols: ["expression", "comp_op", "expression"],
-        postprocess: ([lhs, op, rhs]) =>
-          op === "equals" ? lhs === rhs : lhs !== rhs,
+const grammar = {
+  Lexer: lexer,
+  ParserRules: [
+    {
+      name: "comparison",
+      symbols: ["expression", "comp_op", "expression"],
+      postprocess: ([lhs, op, rhs]) =>
+        op === "equals" ? lhs === rhs : lhs !== rhs,
+    },
+    { name: "expression$ebnf$1", symbols: [] },
+    {
+      name: "expression$ebnf$1$subexpression$1",
+      symbols: ["add_op", "term"],
+    },
+    {
+      name: "expression$ebnf$1",
+      symbols: ["expression$ebnf$1", "expression$ebnf$1$subexpression$1"],
+      postprocess: function arrpush(d) {
+        return d[0].concat([d[1]]);
       },
-      { name: "expression$ebnf$1", symbols: [] },
-      {
-        name: "expression$ebnf$1$subexpression$1",
-        symbols: ["add_op", "term"],
+    },
+    {
+      name: "expression",
+      symbols: ["term", "expression$ebnf$1"],
+      postprocess: ([first, rest]) =>
+        rest.reduce(
+          (acc, [op, val]) => (op === "plus" ? acc + val : acc - val),
+          first
+        ),
+    },
+    { name: "term$ebnf$1", symbols: [] },
+    { name: "term$ebnf$1$subexpression$1", symbols: ["mul_op", "factor"] },
+    {
+      name: "term$ebnf$1",
+      symbols: ["term$ebnf$1", "term$ebnf$1$subexpression$1"],
+      postprocess: function arrpush(d) {
+        return d[0].concat([d[1]]);
       },
-      {
-        name: "expression$ebnf$1",
-        symbols: ["expression$ebnf$1", "expression$ebnf$1$subexpression$1"],
-        postprocess: function arrpush(d) {
-          return d[0].concat([d[1]]);
-        },
+    },
+    {
+      name: "term",
+      symbols: ["factor", "term$ebnf$1"],
+      postprocess: ([first, rest]) =>
+        rest.reduce(
+          (acc, [op, val]) => (op === "multiply" ? acc * val : acc / val),
+          first
+        ),
+    },
+    {
+      name: "factor",
+      symbols: ["number"],
+      postprocess: ([value]) => parseFloat(value),
+    },
+    {
+      name: "factor",
+      symbols: [{ literal: "(" }, "expression", { literal: ")" }],
+      postprocess: ([, exp]) => exp,
+    },
+    {
+      name: "add_op",
+      symbols: [{ literal: "+" }],
+      postprocess: ([op]) => "plus",
+    },
+    {
+      name: "add_op",
+      symbols: [{ literal: "-" }],
+      postprocess: ([op]) => "minus",
+    },
+    {
+      name: "mul_op",
+      symbols: [{ literal: "*" }],
+      postprocess: ([op]) => "multiply",
+    },
+    {
+      name: "mul_op",
+      symbols: [{ literal: "/" }],
+      postprocess: ([op]) => "divide",
+    },
+    {
+      name: "comp_op",
+      symbols: [{ literal: "=" }],
+      postprocess: ([op]) => "equals",
+    },
+    {
+      name: "comp_op",
+      symbols: [{ literal: "!=" }],
+      postprocess: ([op]) => "notEquals",
+    },
+    { name: "number$ebnf$1", symbols: [/[0-9]/] },
+    {
+      name: "number$ebnf$1",
+      symbols: ["number$ebnf$1", /[0-9]/],
+      postprocess: function arrpush(d) {
+        return d[0].concat([d[1]]);
       },
-      {
-        name: "expression",
-        symbols: ["term", "expression$ebnf$1"],
-        postprocess: ([first, rest]) =>
-          rest.reduce(
-            (acc, [op, val]) => (op === "plus" ? acc + val : acc - val),
-            first
-          ),
-      },
-      { name: "term$ebnf$1", symbols: [] },
-      { name: "term$ebnf$1$subexpression$1", symbols: ["mul_op", "factor"] },
-      {
-        name: "term$ebnf$1",
-        symbols: ["term$ebnf$1", "term$ebnf$1$subexpression$1"],
-        postprocess: function arrpush(d) {
-          return d[0].concat([d[1]]);
-        },
-      },
-      {
-        name: "term",
-        symbols: ["factor", "term$ebnf$1"],
-        postprocess: ([first, rest]) =>
-          rest.reduce(
-            (acc, [op, val]) => (op === "multiply" ? acc * val : acc / val),
-            first
-          ),
-      },
-      {
-        name: "factor",
-        symbols: ["number"],
-        postprocess: ([value]) => parseFloat(value),
-      },
-      {
-        name: "factor",
-        symbols: [{ literal: "(" }, "expression", { literal: ")" }],
-        postprocess: ([, exp]) => exp,
-      },
-      {
-        name: "add_op",
-        symbols: [{ literal: "+" }],
-        postprocess: ([op]) => "plus",
-      },
-      {
-        name: "add_op",
-        symbols: [{ literal: "-" }],
-        postprocess: ([op]) => "minus",
-      },
-      {
-        name: "mul_op",
-        symbols: [{ literal: "*" }],
-        postprocess: ([op]) => "multiply",
-      },
-      {
-        name: "mul_op",
-        symbols: [{ literal: "/" }],
-        postprocess: ([op]) => "divide",
-      },
-      {
-        name: "comp_op",
-        symbols: [{ literal: "=" }],
-        postprocess: ([op]) => "equals",
-      },
-      {
-        name: "comp_op",
-        symbols: [{ literal: "!=" }],
-        postprocess: ([op]) => "notEquals",
-      },
-      { name: "number$ebnf$1", symbols: [/[0-9]/] },
-      {
-        name: "number$ebnf$1",
-        symbols: ["number$ebnf$1", /[0-9]/],
-        postprocess: function arrpush(d) {
-          return d[0].concat([d[1]]);
-        },
-      },
-      { name: "number", symbols: ["number$ebnf$1"] },
-      { name: "whitespace", symbols: [{ literal: " " }] },
-    ],
-    ParserStart: "comparison",
-  };
-  if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
-    module.exports = grammar;
-  } else {
-    window.grammar = grammar;
-  }
-})();
+    },
+    { name: "number", symbols: ["number$ebnf$1"] },
+    { name: "whitespace", symbols: [{ literal: " " }] },
+  ],
+  ParserStart: "comparison",
+};
 
 export default grammar;
